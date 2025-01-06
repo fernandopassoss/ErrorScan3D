c0e4603f8190fad2cea665dc39b9cf90066214db
Fernando <73785564+fernandopassoss@users.noreply.github.com>
2025-01-06 11:22:30 -0300

    ajustes em home

--- errorscan3d/src/pages/Home.jsx
+++ errorscan3d/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import '../styles/Home.scss'
+import snake from '../assets/snake.png';
+import circle from '../assets/circle1.png'
+
 
 
 function Home() {
@@ -17,6 +20,7 @@ function Home() {
             <p className='container1-texto'>
               Esta aplicação foi desenvolvida para ajudar você a identificar problemas em impressões 3D de forma rápida e precisa.
             </p>
+            <img src={snake} alt="" className='container1-img'/>
           </div>
 
         </div>
@@ -24,6 +28,7 @@ function Home() {
           <p className='container2-texto'>
             Envie uma imagem, visualize os resultados e otimize seus processos de fabricação!
           </p>
+          <img src={circle} alt="" className='container2-img'/>
         </div>
 
 
