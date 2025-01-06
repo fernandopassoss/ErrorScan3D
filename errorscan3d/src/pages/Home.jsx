df2bd47c6f02b3de754515bfc6624be0fe0cdc3c
Fernando <73785564+fernandopassoss@users.noreply.github.com>
2025-01-06 10:37:33 -0300

    novas alterações

--- errorscan3d/src/pages/Home.jsx
+++ errorscan3d/src/pages/Home.jsx
@@ -2,26 +2,30 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import '../styles/Home.scss'
 
+
 function Home() {
   return (
-    <div>
-
+    <div className='home-fundo' >
+      
       <NavBar />
       <div className='home'>
-
-        
         <div className='home-container1'>
-          <p>
-            Bem-vindo ao ErrorScan3D
-          </p>
-          <p>
-            Esta aplicação foi desenvolvida para ajudar você a identificar problemas em impressões 3D de forma rápida e precisa.
-          </p>
-        </div>
-        <div className='home-container-2'>
+          <div className='container1-conteudo'>
+            <p className='container1-titulo'>
+              Bem-vindo ao ErrorScan3D
+            </p>
+            <p className='container1-texto'>
+              Esta aplicação foi desenvolvida para ajudar você a identificar problemas em impressões 3D de forma rápida e precisa.
+            </p>
+          </div>
 
         </div>
-        
+        <div className='home-container2'>
+          <p className='container2-texto'>
+            Envie uma imagem, visualize os resultados e otimize seus processos de fabricação!
+          </p>
+        </div>
+
 
       </div>
     </div>
